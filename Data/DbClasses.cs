146a45dd5ff054fdae64e0b4b0294b6c56ade547
Senior-toreodor <eugenstramousov@gmail.com>
2025-09-10 13:17:10 +0200

    Sorting in wpf app

---
+++ Data/DbClasses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BrightTestTask.Data
+{
+    public class NumbersTable
+    {
+        public int Id { get; set; }
+        public int Value { get; set; } 
+    }
+
+    public class SortedNumbersTable
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public int Position { get; set; }
+    }
+
+    public class NumbersDbContext : DbContext
+    {
+        public NumbersDbContext()
+            : base(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=NumbersDb_CodeFirst;Integrated Security=True;MultipleActiveResultSets=True")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<NumbersDbContext>());
+        }
+
+        public DbSet<NumbersTable> Numbers { get; set; } = null!;
+        public DbSet<SortedNumbersTable> SortedNumbers { get; set; } = null!;
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BrightTestTask.Data;
+
+namespace BrightTestTask
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var db = new NumbersDbContext())
+            {
+                db.Database.Initialize(false);
+            }
+        }
+
+        private void GenerateData(object sender, RoutedEventArgs e)
+        {
+            using var db = new NumbersDbContext();
+
+            db.Numbers.RemoveRange(db.Numbers);
+            db.SortedNumbers.RemoveRange(db.SortedNumbers);
+            db.SaveChanges();
+
+            var random = new Random();
+            var values = Enumerable.Range(1, 10).OrderBy(x => random.Next()).ToList();
+
+            var numbers = new List<NumbersTable>();
+            for (int i = 0; i < values.Count; i++) 
+                numbers.Add(new NumbersTable { Value = values[i] }); 
+
+            db.Numbers.AddRange(numbers);
+            db.SaveChanges();
+
+            NumbersGrid.ItemsSource = db.Numbers.ToList();
+            SortedGrid.ItemsSource = db.SortedNumbers.ToList(); 
+
+            StatusText.Text = "Table 1 filled.";
+        }
+
+        private void SortCopyData(object sender, RoutedEventArgs e)
+        {
+            using var db = new NumbersDbContext();
+
+            db.SortedNumbers.RemoveRange(db.SortedNumbers);
+            db.SaveChanges();
+                 
+            var sorted = db.Numbers
+                .Select(n => new { n.Id, n.Value })    
+                .OrderBy(n => n.Value)               
+                .ToList();
+
+             var toInsert = new List<SortedNumbersTable>(sorted.Count);
+             for (int i = 0; i < sorted.Count; i++)
+             {
+                 var item = sorted[i];
+                 toInsert.Add(new SortedNumbersTable{ Value = item.Value, Position = item.Id });
+             }
+
+             db.SortedNumbers.AddRange(toInsert);
+             db.SaveChanges();
+              
+             SortedGrid.ItemsSource = db.SortedNumbers.OrderBy(s => s.Value).ToList();
+             StatusText.Text = "Table 2 filled and sorted."; 
+        }
+    }
+}
